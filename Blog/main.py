d8fbcb0f008c6a7982e8339eaf2eacc9610cb8ca
Kyle Sammons <goodkingkyle@gmail.com>
2018-09-13 21:16:02 -0700

    Fixed stuff

--- Blog/main.py
+++ Blog/main.py
@@ -11,28 +11,19 @@ from os.path import isfile, join
 app = Flask(__name__)
 POST_DIR = 'posts'
 
-def getPosts():
-    return [join(POST_DIR, f) for f in listdir(POST_DIR) if isfile(join(POST_DIR, f))]
-
 @app.route("/")
 def root():
-    posts = getPosts()[:3]
-    nums = {
-        0: "first",
-        1: "second",
-        2: "third"
-    }
-    replacements = {}
-    for i, post in enumerate(posts):
-        with open(post) as file:
-            replacements[nums[i]] = Markup(markdown.markdown(file.read()))
+    filenames = [join(POST_DIR, f) for f in listdir(POST_DIR) if isfile(join(POST_DIR, f))]
+    first_three = filenames[:3]
+    posts = []
+    for filename in first_three:
+        with open(filename) as file:
+            posts.append(Markup(markdown.markdown(file.read())))
 
-    return render_template('index.html', **replacements)
-
-
-
+    return render_template('index.html', posts=posts)
 
 if __name__ == "__main__":
     if len(sys.argv) == 2 and sys.argv[1] == "-d":
         app.run(debug=True, host='0.0.0.0', port=28080)
+
     app.run(debug=False, port=80)
